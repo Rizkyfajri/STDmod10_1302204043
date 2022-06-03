77bbfa37b7c6321690ba0171469a3e19026902d7
Rizky Fajri <mansamussolini@gmail.com>
2022-06-03 09:48:50 +0700

    isEmpty added

--- mod10_1302204043/Stack.cpp
+++ mod10_1302204043/Stack.cpp
@@ -1,5 +1,15 @@
 #include "Stack.h"
 
 void create_stack(stack& s) {
-	Top(s) = -1;
+	top(s) = -1;
 }
+
+bool is_empty(stack s) {
+	if (top(s) == -1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
