010daf815858abdf45457e091f17d2f55e087dc4
Rizky Fajri <mansamussolini@gmail.com>
2022-06-03 09:44:33 +0700

    Add project files.

---
+++ mod10_1302204043/Stack.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+typedef char infotype;
+//Muhammad Rizky Fajri 1302204043
+struct stack {
+	infotype tabS[10];
+	int top;
+};
+
+void create_stack(stack& s);
+bool is_empty(stack s);
+bool is_full(stack s);
+void push(stack& s, infotype x);
+void pop(stack& s, infotype x);
+void terima_output(stack& s);
+void print_info(stack s);
+
